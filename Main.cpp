788fa60d0f7e1846528d6366a5047db6b58e6c04
Maikruts Ilya Oleksandrovych <143324723+1337rave@users.noreply.github.com>
2023-12-28 21:11:02 +0200

    done

---
+++ Main.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+
+class Date {
+private:
+    int day;
+    int month;
+    int year;
+
+public:
+    Date(int d, int m, int y) : day(d), month(m), year(y) {}
+
+    // Перевантаження оператору "-"
+    int operator-(const Date& other) const {
+        const int days_in_month[] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+        int days = 0;
+
+        // Додавання днів з минулих років
+        for (int i = other.year; i < year; ++i) {
+            days += isLeapYear(i) ? 366 : 365;
+        }
+
+        // Додавання днів з поточного року
+        for (int i = 1; i < month; ++i) {
+            days += days_in_month[i];
+        }
+        days += day;
+
+        // Віднімання днів з іншого року
+        for (int i = 1; i < other.month; ++i) {
+            days -= days_in_month[i];
+        }
+        days -= other.day;
+
+        return days;
+    }
+
+    // Перевантаження оператору "+"
+    Date operator+(int days) const {
+        Date result = *this;
+        result.day += days;
+
+        while (result.day > daysInMonth(result.month, result.year)) {
+            result.day -= daysInMonth(result.month, result.year);
+            ++result.month;
+
+            if (result.month > 12) {
+                result.month = 1;
+                ++result.year;
+            }
+        }
+
+        return result;
+    }
+
+    // Перевірка на високосний рік
+    bool isLeapYear(int y) const {
+        return (y % 4 == 0 && y % 100 != 0) || (y % 400 == 0);
+    }
+
+    // Кількість днів у місяці
+    int daysInMonth(int m, int y) const {
+        const int days_in_month[] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        if (m == 2 && isLeapYear(y))
+            return 29;
+        return days_in_month[m];
+    }
+
+    // Виведення дати
+    void display() const {
+        std::cout << year << "-" << month << "-" << day;
+    }
+};
+
+int main() {
+    int day1, month1, year1;
+    int day2, month2, year2;
+
+    // Введення першої дати
+    std::cout << "Enter the first date (day month year): ";
+    std::cin >> day1 >> month1 >> year1;
+    Date date1(day1, month1, year1);
+
+    // Введення другої дати
+    std::cout << "Enter the second date (day month year): ";
+    std::cin >> day2 >> month2 >> year2;
+    Date date2(day2, month2, year2);
+
+    char choice;
+
+    // Вибір операції
+    std::cout << "Choose an operation (- for difference, + for addition): ";
+    std::cin >> choice;
+
+    int result;
+
+    // Виконання вибраної операції
+    if (choice == '-') {
+        result = date1 - date2;
+        std::cout << "Difference in days: " << result << std::endl;
+    }
+    else if (choice == '+') {
+        int daysToAdd;
+        std::cout << "Enter the number of days to add: ";
+        std::cin >> daysToAdd;
+        Date newDate = date1 + daysToAdd;
+        std::cout << "New date after addition: ";
+        newDate.display();
+        std::cout << std::endl;
+    }
+    else {
+        std::cerr << "Error: Invalid operation." << std::endl;
+        return 1;
+    }
+
+    return 0;
+}
